08d4422fb592b1aa66adc2efb1192875c2efe11f
look-at-the-sky <47905050+look-at-the-sky@users.noreply.github.com>
2022-03-03 05:33:08 +0100

    Add files via upload

---
+++ del_videos.cmd
@@ -0,0 +1,7 @@
+pause
+DEL /F /S /Q %LocalAppData%\osu!\Songs\*.mp4
+DEL /F /S /Q %LocalAppData%\osu!\Songs\*.avi
+DEL /F /S /Q %LocalAppData%\osu!\Songs\*.flv
+DEL /F /S /Q %LocalAppData%\osu!\Songs\*.wmv
+DEL /F /S /Q %LocalAppData%\osu!\Songs\*.m4v
+DEL /F /S /Q %LocalAppData%\osu!\Songs\*.mpg
