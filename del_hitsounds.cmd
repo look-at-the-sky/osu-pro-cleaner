a7125614a557b88a0dac2a5679e62e2b52de0b25
look-at-the-sky <47905050+look-at-the-sky@users.noreply.github.com>
2022-03-04 12:49:06 +0100

    Add files via upload

---
+++ del_hitsounds.cmd
@@ -0,0 +1,7 @@
+pause
+DEL /F /S /Q %LocalAppData%\osu!\Songs\normal-*.wav
+DEL /F /S /Q %LocalAppData%\osu!\Songs\soft-*.wav
+DEL /F /S /Q %LocalAppData%\osu!\Songs\drum-*.wav
+DEL /F /S /Q %LocalAppData%\osu!\Songs\normal-*.ogg
+DEL /F /S /Q %LocalAppData%\osu!\Songs\soft-*.ogg
+DEL /F /S /Q %LocalAppData%\osu!\Songs\drum-*.ogg
