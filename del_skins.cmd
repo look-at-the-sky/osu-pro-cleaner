9d0b82c6d7ed52604071d29ce7348706be10f737
look-at-the-sky <47905050+look-at-the-sky@users.noreply.github.com>
2022-03-04 13:08:11 +0100

    Add files via upload

---
+++ del_skins.cmd
@@ -0,0 +1,9 @@
+pause
+DEL /F /S /Q %LocalAppData%\osu!\Songs\default-*.png
+DEL /F /S /Q %LocalAppData%\osu!\Songs\followpoint-*.png
+DEL /F /S /Q %LocalAppData%\osu!\Songs\hit*.png
+DEL /F /S /Q %LocalAppData%\osu!\Songs\approachcircle.png
+DEL /F /S /Q %LocalAppData%\osu!\Songs\reversearrow*.png
+DEL /F /S /Q %LocalAppData%\osu!\Songs\scorebar-*.png
+DEL /F /S /Q %LocalAppData%\osu!\Songs\slider*.png
+DEL /F /S /Q %LocalAppData%\osu!\Songs\cursor.png
