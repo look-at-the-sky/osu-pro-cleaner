e9977a5d501eb6c6bb41e3d7c5b802a714d5f254
look-at-the-sky <47905050+look-at-the-sky@users.noreply.github.com>
2022-03-19 17:10:13 +0100

    Update del_hitsounds.cmd

--- del_hitsounds.cmd
+++ del_hitsounds.cmd
@@ -1,7 +1,5 @@
 pause
-DEL /F /S /Q %LocalAppData%\osu!\Songs\normal-*.wav
-DEL /F /S /Q %LocalAppData%\osu!\Songs\soft-*.wav
-DEL /F /S /Q %LocalAppData%\osu!\Songs\drum-*.wav
+DEL /F /S /Q %LocalAppData%\osu!\Songs\*.wav
 DEL /F /S /Q %LocalAppData%\osu!\Songs\normal-*.ogg
 DEL /F /S /Q %LocalAppData%\osu!\Songs\soft-*.ogg
 DEL /F /S /Q %LocalAppData%\osu!\Songs\drum-*.ogg
