86eda0bea68180d5e8205c2bb6713d8399aa501b
look-at-the-sky <47905050+look-at-the-sky@users.noreply.github.com>
2022-06-13 01:07:31 +0200

    Delete del_videos.cmd

--- del_videos.cmd
+++
@@ -1,7 +0,0 @@
-pause
-DEL /F /S /Q %LocalAppData%\osu!\Songs\*.mp4
-DEL /F /S /Q %LocalAppData%\osu!\Songs\*.avi
-DEL /F /S /Q %LocalAppData%\osu!\Songs\*.flv
-DEL /F /S /Q %LocalAppData%\osu!\Songs\*.wmv
-DEL /F /S /Q %LocalAppData%\osu!\Songs\*.m4v
-DEL /F /S /Q %LocalAppData%\osu!\Songs\*.mpg
