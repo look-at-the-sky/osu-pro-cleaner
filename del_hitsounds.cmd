2f6fad05c2496d68c8dd99d93fbd40894f06a45a
look-at-the-sky <47905050+look-at-the-sky@users.noreply.github.com>
2022-06-13 01:07:10 +0200

    Delete del_hitsounds.cmd

--- del_hitsounds.cmd
+++
@@ -1,5 +0,0 @@
-pause
-DEL /F /S /Q %LocalAppData%\osu!\Songs\*.wav
-DEL /F /S /Q %LocalAppData%\osu!\Songs\normal-*.ogg
-DEL /F /S /Q %LocalAppData%\osu!\Songs\soft-*.ogg
-DEL /F /S /Q %LocalAppData%\osu!\Songs\drum-*.ogg
