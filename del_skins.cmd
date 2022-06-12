a96e463c4fbdbf02e1ce2c9249a1d87b14359173
look-at-the-sky <47905050+look-at-the-sky@users.noreply.github.com>
2022-06-13 01:07:26 +0200

    Delete del_skins.cmd

--- del_skins.cmd
+++
@@ -1,9 +0,0 @@
-pause
-DEL /F /S /Q %LocalAppData%\osu!\Songs\default-*.png
-DEL /F /S /Q %LocalAppData%\osu!\Songs\followpoint-*.png
-DEL /F /S /Q %LocalAppData%\osu!\Songs\hit*.png
-DEL /F /S /Q %LocalAppData%\osu!\Songs\approachcircle.png
-DEL /F /S /Q %LocalAppData%\osu!\Songs\reversearrow*.png
-DEL /F /S /Q %LocalAppData%\osu!\Songs\scorebar-*.png
-DEL /F /S /Q %LocalAppData%\osu!\Songs\slider*.png
-DEL /F /S /Q %LocalAppData%\osu!\Songs\cursor.png
